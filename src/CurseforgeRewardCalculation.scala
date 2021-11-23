655d4b42c30d070bf88f486d31efaa7073e3666e
MikhailTapio <mikhailtapio13468@163.com>
2021-11-23 22:22:03 +0800

    21.11.23/22:21

---
+++ src/CurseforgeRewardCalculation.scala
@@ -0,0 +1,47 @@
+package com.plr.scaladev
+
+import java.util.Scanner
+import scala.language.postfixOps
+
+object CurseforgeRewardCalculation {
+
+  def input(scanner:Scanner): Unit ={
+    println("欢迎使用 Curseforge 点数奖励价值评估系统")
+    println("请输入兑换方案 A 需要的点数")
+    val aCost = scanner.nextInt()
+    println("请输入方案 A 可兑换的货币数目")
+    val aGain = scanner.nextInt()
+    println("请输入方案 A 兑换的货币对人民币的理论汇率（接受小数）")
+    val aRespRmb = scanner.nextDouble()
+    println("请输入兑换方案 B 需要的点数")
+    val bCost = scanner.nextInt()
+    println("请输入方案 B 可兑换的货币数目")
+    val bGain = scanner.nextInt()
+    println("请输入方案 B 兑换的货币对人民币的理论汇率（接受小数）")
+    val bRespRmb = scanner.nextDouble()
+    println("计算中")
+    print(".")
+    print(".")
+    print(".")
+    print(".")
+    print(".")
+    print(".")
+    println("")
+    val calculated = calculateAndCompare(aCost, aGain, aRespRmb, bCost, bGain, bRespRmb)
+    println(calculated)
+  }
+
+  private def calculateAndCompare(aNeededPts: Int,
+                                  aGainsCurrency: Int,
+                                  aRespectiveCurrencyToRMB: Double,
+                                  bNeededPts: Int,
+                                  bGainsCurrency: Int,
+                                  bRespectiveCurrencyToRMB: Double
+                                 ): String ={
+    if (aGainsCurrency * aRespectiveCurrencyToRMB / aNeededPts >= bGainsCurrency * bRespectiveCurrencyToRMB / bNeededPts)
+      "A方案更划算，平均每pt兑换%f人民币".format(aGainsCurrency * aRespectiveCurrencyToRMB / aNeededPts)
+    else
+      "B方案更划算，平均每pt兑换%f人民币".format(bGainsCurrency * bRespectiveCurrencyToRMB / bNeededPts)
+  }
+
+}
--- src/Main.scala
+++ src/Main.scala
@@ -1,26 +1,32 @@
 package com.plr.scaladev
 
 import java.util.Scanner
+import scala.util.control.Breaks
 
 object Main {
 
   def main(args: Array[String]): Unit = {
+    println("欢迎使用 Tapio 的工具集！")
     val scanner = new Scanner(System.in)
-    println("欢迎使用 Curseforge 点数奖励价值评估系统")
-    println("请输入兑换方案 A 需要的点数")
-    val aCost = scanner.nextInt()
-    println("请输入方案 A 可兑换的货币数目")
-    val aGain = scanner.nextInt()
-    println("请输入方案 A 兑换的货币对人民币的理论汇率（接受小数）")
-    val aRespRmb = scanner.nextDouble()
-    println("请输入兑换方案 B 需要的点数")
-    val bCost = scanner.nextInt()
-    println("请输入方案 B 可兑换的货币数目")
-    val bGain = scanner.nextInt()
-    println("请输入方案 B 兑换的货币对人民币的理论汇率（接受小数")
-    val bRespRmb = scanner.nextDouble()
-    println("计算中......")
-    val calculated = CalculateAndCompareValue.calculate(aCost, aGain, aRespRmb, bCost, bGain, bRespRmb)
-    println(calculated)
+    showCategories()
+    var selection = 0
+    while (selection >= 0) {
+      selection = if (scanner.hasNextInt()) scanner.nextInt() else 0
+      selection match {
+        case 1 =>
+          CurseforgeRewardCalculation.input(scanner)
+        case 0 => selection = -1
+        case _ =>
+          showCategories()
+          selection = 0
+      }
+    }
+    scanner.close()
+  }
+
+  def showCategories(): Unit ={
+    println("输入序号以使用功能")
+    println("1. Curseforge 点数奖励价值评估系统")
+    println("0. 退出程序")
   }
 }
